eed456a2d2e3830d1a3f7817645545012e811be9
Arun <arunkumaryadav9@gmail.com>
2023-08-19 10:20:10 +0530

    Added delete and exit button

--- gui.py
+++ gui.py
@@ -8,9 +8,16 @@ list_box = sg.Listbox(values=functions.get_todos(), key='todos',
                       enable_events=True, size=[45, 10])
 edit_button =sg.Button("Edit")
 
+delete_button = sg.Button("Delete")
+
+exit_button = sg.Button("Exit")
+
 window = sg.Window('My To-Do App',
-                   layout = [[label],[input_box, add_button], [list_box,edit_button]],
-                   font = ('Helvetica',20))
+                   layout = [[label],
+                             [input_box, add_button],
+                             [list_box,edit_button, delete_button],
+                             [exit_button]],
+                            font = ('Helvetica',20))
 
 while True:
     event, values = window.read()
@@ -22,6 +29,7 @@ while True:
             new_todo = values['todo'] + '\n'
             todos.append(new_todo)
             functions.write_todos(todos)
+            window['todos'].update(values=todos)
         case 'Edit':
             todo_to_edit = values['todos'][0]
             new_todo = values['todo'] + '\n'
@@ -32,6 +40,15 @@ while True:
             window['todos'].update(values=todos)
         case 'todos':
             window['todo'].update(value=values['todos'][0])
+        case 'Delete':
+            todo_to_delete = values['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_delete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
+        case 'Exit':
+            break
         case sg.WIN_CLOSED:
             break
 window.close()
